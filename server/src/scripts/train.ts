35dcc550f24cc6616ad6964db5f1a47eb964d3c4
Brian Pham <nguyenphamswork@gmail.com>
2025-06-28 15:37:19 -0700

    adding new training iamges

--- server/src/scripts/train.ts
+++ server/src/scripts/train.ts
@@ -3,9 +3,9 @@ import { promises as fs } from "fs";
 import path from "path";
 
 async function perform() {
-  let png = await fs.readFile(path.join(__dirname, "..", "assets", "CardplusCompasslogo.png"));
+  let png = await fs.readFile(path.join(__dirname, "..", "assets", "drawing.png"));
   let target = await train(png);
-  await fs.writeFile(path.join(__dirname, "..", "assets", "CardplusCompasslogo.zpt"), target);
+  await fs.writeFile(path.join(__dirname, "..", "assets", "drawing.zpt"), target);
 }
 
 perform();
