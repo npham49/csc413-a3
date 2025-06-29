1ea2381af727c1a280159d1d8406083d1d500da2
hieuvuong2310 <hieuvuong2310@gmail.com>
2025-06-29 04:27:26 -0700

    Add button and handling all logic done

--- rfidscan/a3/a3.ino
+++ rfidscan/a3/a3.ino
@@ -9,9 +9,11 @@
 #define NR_OF_READERS 2
 byte ssPins[] = {SS_1_PIN, SS_2_PIN};
 MFRC522 mfrc522[NR_OF_READERS];
-
+const int buttonPin = 5;  // the number of the pushbutton pin
+int buttonState = 0;
 void setup() {
   Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
   while (!Serial)
     ; // Wait for serial connection (for Leonardo/Micro)
 
@@ -28,6 +30,10 @@ void setup() {
 }
 
 void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Serial.println("Done");
+  } 
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     if (mfrc522[reader].PICC_IsNewCardPresent() &&
         mfrc522[reader].PICC_ReadCardSerial()) {
@@ -43,15 +49,24 @@ void loop() {
       Serial.print(tagID);
       Serial.println();
       Serial.print(reader);
-      if (tagID == 2713759238 || tagID == 2976165638) {
+      if (tagID == 2983046662) {
         Serial.print(" Top: Shirt");
-      } else if (tagID == 4056264198 || tagID == 1902292742) {
+      } else if (tagID == 1371713030) {
         Serial.print(" Top: TShirt");
-      } else if (tagID == 2439032582 || tagID == 3776032262) {
+      } else if (tagID == 3780284745) {
         Serial.print(" Bottom: Pants");
-      } else if (tagID == 22916870 || tagID == 566537734) {
+      } else if (tagID == 3519786502) {
         Serial.print(" Bottom: Skirt");
-      } 
+      } else if (tagID == 3776032262) {
+        Serial.print(" Color: Red");
+      } else if (tagID == 4056264198) {
+        Serial.print(" Color: Green");
+      } else if (tagID == 2976165638) {
+        Serial.print(" Color: Blue");
+      } else if (tagID == 2439032582) {
+        Serial.print(" Color: Black");
+      }
+
       Serial.println();
       mfrc522[reader].PICC_HaltA();      // Halt the card
       mfrc522[reader].PCD_StopCrypto1(); // Stop encryption
